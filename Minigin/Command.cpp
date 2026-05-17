c389d48339858631405b419a2532362a78628372
Yasmin Cauwenbergh <Yasmin.cauwenbergh@student.howest.be>
2026-05-17 10:14:16 +0200

    fixing unused variable error

--- Minigin/Command.cpp
+++ Minigin/Command.cpp
@@ -76,6 +76,7 @@ namespace dae {
 	{
 		assert(m_ObjectsScoreComponent != nullptr && "Healthcomponent was a nullpointer in the command execute.");
 		//m_ObjectsScoreComponent->AddScorePoints(m_AmountScoreChange);
+		m_AmountScoreChange++;
 		
 	}
 
